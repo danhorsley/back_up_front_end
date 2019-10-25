526ae3d021379af65fc389efb051754c19e4895c
danielhorsley <danielhorsley@users.noreply.github.com>
2019-10-25 11:58:52 +0100

    workign movement buttons

--- grid.py
+++ grid.py
@@ -1,10 +1,19 @@
 import arcade
 from queries import *
+from buttons import *
 
-floor_number = input('please input floor number : ')
-floor_number = int(floor_number)
+
+auth_key = login()
+look_info = look()
+floor_number = look_info['floor']
+#floor_number = input('please input floor number : ')
+#floor_number = int(floor_number)
 arq = heroku_query(all_rooms_query)['data']['rooms']
 arq0 = [x for x in arq if x['floor'] == floor_number]
+
+player_loc_x = look_info['x']
+player_loc_y = look_info['y']
+#print(player_loc_x, player_loc_y)
 #print(arq0[0])
 
 
@@ -36,10 +45,15 @@ SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN + 200
 SCREEN_HEIGHT = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN
 SCREEN_TITLE = "MUD Game"
 
+#buttons define
+button_list = []
+
+
 class MyGame(arcade.Window):
     """
     Main application class.
     """
+
 
     def __init__(self, width, height, title):
         """
@@ -51,6 +65,10 @@ class MyGame(arcade.Window):
         # Create a 2 dimensional array. A two dimensional
         # array is simply a list of lists.
         self.grid = []
+        self.x = player_loc_x
+        self.y = player_loc_y 
+        self.floor = floor_number
+
         for row in range(ROW_COUNT):
             # Add an empty array that will hold each cell
             # in this row
@@ -59,6 +77,23 @@ class MyGame(arcade.Window):
                 self.grid[row].append(0)  # Append a cell
 
         arcade.set_background_color(arcade.color.BLACK)
+        
+
+    def setup(self):
+        
+        self.button_list = []
+        north_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 100)
+        self.button_list.append(north_button)
+        south_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 150,action_function = move_s, text = "South")
+        self.button_list.append(south_button)
+        west_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 200,action_function = move_w, text = "West")
+        self.button_list.append(west_button)
+        east_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 250,action_function = move_e, text = "East")
+        self.button_list.append(east_button)
+        up_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 300,action_function = move_u, text = "Up")
+        self.button_list.append(up_button)
+        down_button = DirTextButton(SCREEN_WIDTH - 100, SCREEN_HEIGHT - 350,action_function = move_d, text = "Down")
+        self.button_list.append(down_button)
 
     def on_draw(self):
         """
@@ -102,52 +137,76 @@ class MyGame(arcade.Window):
                         m = directions[k]
                         arcade.draw_rectangle_filled(x +m[0], y +m[1] , m[2], m[3], my_color[color])   
 
-                
+            #draw player location 
+            
+        arcade.draw_rectangle_filled( (MARGIN + WIDTH) * self.x + MARGIN + WIDTH // 2, 
+                                (MARGIN + HEIGHT) * self.y + MARGIN + HEIGHT // 2 , 10, 10, arcade.color.WHITE)  
 
-    def on_mouse_press(self, x, y, button, modifiers):
+        for button in self.button_list:
+            button.draw()
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
         """
         Called when the user presses a mouse button.
         """
+        check_mouse_press_for_buttons(x, y, self.button_list)
 
-        # Change the x/y screen coordinates to grid coordinates
-        column = int(x // (WIDTH + MARGIN))
-        row = int(y // (HEIGHT + MARGIN))
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        check_mouse_release_for_buttons(x, y, self.button_list)
+        new_look = look()
+        self.x = new_look['x']
+        self.y = new_look['y']
+        self.floor = new_look['floor']
+                
 
-        print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
-        room_ref = [c for c in arq0 if c['x'] == column and c['y'] == row][0]
-        print(room_ref)
+    # def on_mouse_press(self, x, y, button, modifiers):
+    #     """
+    #     Called when the user presses a mouse button.
+    #     """
 
-        room_description = room_ref['description']
+    #     # Change the x/y screen coordinates to grid coordinates
+    #     column = int(x // (WIDTH + MARGIN))
+    #     row = int(y // (HEIGHT + MARGIN))
 
-        n=50
-        room_description = ' /n '.join([room_description[i:i+n] for i in range(0, len(room_description), n)])
+    #     print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({row}, {column})")
+    #     room_ref = [c for c in arq0 if c['x'] == column and c['y'] == row][0]
+    #     print(room_ref)
 
-        room_itemdesc = room_ref['itemdesc']
+    #     room_description = room_ref['description']
 
-        desc_text_loc_x = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN + 3
-        desc_text_loc_y = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN - 50
+    #     n=50
+    #     room_description = ' /n '.join([room_description[i:i+n] for i in range(0, len(room_description), n)])
 
-        arcade.draw_lrtb_rectangle_outline(desc_text_loc_x, desc_text_loc_x + 100,
-                                    desc_text_loc_y + 100, desc_text_loc_y,
-                                    arcade.color.WHITE, 1)
+    #     room_itemdesc = room_ref['itemdesc']
+
+    #     desc_text_loc_x = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN + 3
+    #     desc_text_loc_y = (HEIGHT + MARGIN) * ROW_COUNT + MARGIN - 50
+
+    #     arcade.draw_lrtb_rectangle_outline(desc_text_loc_x, desc_text_loc_x + 100,
+    #                                 desc_text_loc_y + 100, desc_text_loc_y,
+    #                                 arcade.color.WHITE, 1)
         
-        arcade.draw_text(room_description, desc_text_loc_x, desc_text_loc_y, 
-                                        arcade.color.BLUE, 9, align="center")
+    #     arcade.draw_text(room_description, desc_text_loc_x, desc_text_loc_y, 
+    #                                     arcade.color.BLUE, 9, align="center")
 
-        # Make sure we are on-grid. It is possible to click in the upper right
-        # corner in the margin and go to a grid location that doesn't exist
-        if row < ROW_COUNT and column < COLUMN_COUNT:
+    #     # Make sure we are on-grid. It is possible to click in the upper right
+    #     # corner in the margin and go to a grid location that doesn't exist
+    #     if row < ROW_COUNT and column < COLUMN_COUNT:
 
-            # Flip the location between 1 and 0.
-            if self.grid[row][column] == 0:
-                self.grid[row][column] = 1
-            else:
-                self.grid[row][column] = 0
+    #         # Flip the location between 1 and 0.
+    #         if self.grid[row][column] == 0:
+    #             self.grid[row][column] = 1
+    #         else:
+    #             self.grid[row][column] = 0
 
 
 def main():
 
-    MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    game.setup()
     arcade.run()
 
 
--- queries.py
+++ queries.py
@@ -24,7 +24,9 @@ def login(username = 'danh', passwords = 'mudgame19'):
     else:
         raise Exception("Query failed to run by returning code of {}".format(request.status_code))
 
-def look():
+auth_key = login()
+
+def look(auth_key = auth_key):
     auth_key = login()['key']
     #print(auth_key)
     request = requests.post('https://danpatadv.herokuapp.com/look/', headers=\
@@ -34,7 +36,7 @@ def look():
     else:
         raise Exception("Query failed to run by returning code of {}".format(request.status_code))
 
-def move(direct = 'n'):
+def move(auth_key = auth_key,direct = 'n'):
     auth_key = login()['key']
     #print(auth_key)
     request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
@@ -44,11 +46,75 @@ def move(direct = 'n'):
     else:
         raise Exception("Query failed to run by returning code of {}".format(request.status_code))
 
-def pick(obj = ''):
+
+
+def pick(auth_key = auth_key,obj = ''):
     auth_key = login()['key']
     #print(auth_key)
     request = requests.post('https://danpatadv.herokuapp.com/pick', headers=\
 {"Authorization" : f"Token {auth_key}"},json={'pick up' : obj})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+
+def move_n(auth_key = auth_key):
+    #auth_key = login()['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 'n'})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+def move_s(auth_key = auth_key):
+    #auth_key = login()['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 's'})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+
+def move_e(auth_key = auth_key):
+    #auth_key = login(auth_key = auth_key)['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 'e'})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+def move_w(auth_key = auth_key):
+    #auth_key = login()['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 'w'})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+def move_u(auth_key = auth_key):
+    #auth_key = login()['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 'u'})
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception("Query failed to run by returning code of {}".format(request.status_code))
+
+def move_d(auth_key = auth_key):
+    #auth_key = login()['key']
+    #print(auth_key)
+    request = requests.post('https://danpatadv.herokuapp.com/move/', headers=\
+{"Authorization" : f"Token {auth_key}"},json={'direction' : 'd'})
     if request.status_code == 200:
         return request.json()
     else:
